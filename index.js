940f62e24b3920489c0572694bb822ae06704d03
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 16:53:54 -0700

    commit tree f3ce0844c00c8f5094af1cbc503cfdced9112a89

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 function bfs(rootNode, vertices, edges){
+    let queue = []
+    let recordOfSearch = []
 
+    rootNode.distance = 0
+    queue.push(rootNode)
+    
+    while(queue.length > 0){
+        let firstNode = queue.shift()
+        recordOfSearch.push(firstNode)
+        let adjacentVertices = findAdjacent(firstNode.name, vertices, edges)
+
+        markDistanceAndPredecessor(firstNode, adjacentVertices)
+
+        for(let vertex in adjacentVertices){
+            queue.push(adjacentVertices[vertex])
+        }
+    }
+
+    return recordOfSearch
+}
+
+
+function findAdjacent(nodeName, vertices, edges){
+    let adjacentNodes = []
+
+    edges.forEach(edge => {
+        let indexInEdge = edge.indexOf(nodeName)
+
+        if(indexInEdge == 0){
+            adjacentNodes.push(edge[1])
+        }else if(indexInEdge == 1){
+            adjacentNodes.push(edge[0])
+        }
+    })
+
+    return vertices.filter(vertex => { 
+        return adjacentNodes.includes(vertex.name) && vertex.distance === null
+    })
+}
+
+function markDistanceAndPredecessor(predNode, adjacentNodes){
+    adjacentNodes.forEach(node => {
+        node.predecessor = predNode
+        node.distance = predNode.distance + 1
+    })
 }
